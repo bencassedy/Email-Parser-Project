4e4bad510049bd026de2bdb0dd83e81bb353f2bc
Ben Cassedy <bencassedy@hotmail.com>
2014-01-20 21:47:07 -0700

    remove unused files, update base html, templates, app

--- app/app.py
+++ app/app.py
@@ -1,8 +1,21 @@
-from flask import Flask
-from flask.ext.script import Manager
+from flask import render_template
+from pymongo import MongoClient
 
-app = Flask(__name__)
-manager = Manager(app)
+client = MongoClient('localhost', 27017)
+db = client.enron
+emails = db.test_kaminski
 
-if __name__ == '__main__':
-    manager.run()
+@app.route('/')
+@app.route('/index/')
+def index():
+    return render_template('base.html')
+
+@app.route('/emails/<message_id>/')
+def email_detail(message_id):
+    # get email from mongodb using query by id
+    msg = emails.find_one({'Message-ID': message_id}) 
+    return render_template('detail.html', msg=msg)
+
+@app.route('/emails/')
+def email_list():
+    return render_template('list.html')
--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
+from mongoengine import *
 
-class Email(msg):
-    def 
+class Email(Document):
+    tags = ListField(StringField(max_length=30))
+    comments = ListField(EmbeddedDocumentField(Comment))
+
+class User(Document):
+    email = StringField(required=True)
+    first_name = StringField(max_length=50)
+    last_name = StringField(max_length=50)
+
+class Comment(EmbeddedDocument):
+    body = StringField()
+    author = ReferenceField(User)
+
+    meta = {'allow_inheritance': True}
+
+class PrivilegeDescription(Comment):
+    # inherits from Comment class
+    priv_tags = ListField(StringField(max_length=30))
+
