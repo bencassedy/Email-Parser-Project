8f99950fde56d09d686e428f8528bdaab0eb8993
Ben Cassedy <bencassedy@hotmail.com>
2014-01-27 19:47:17 -0700

    additions to config file

---
+++ app/config.py
@@ -0,0 +1,3 @@
+# configuration settings for application
+# LIST_VIEW_FIELDS can be used to set email fields to display in list view.
+LIST_VIEW_FIELDS = ['To', 'From', 'CC', 'BCC', 'Subject', 'Date', 'Message-ID']
