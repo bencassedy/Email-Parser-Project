daac370121a133c1771ddec77e66662ed308ac75
Ben Cassedy <bencassedy@hotmail.com>
2014-02-13 23:08:26 -0700

    fixed adv search, new app routes, need to fix mlt

--- app/app.py
+++ app/app.py
@@ -53,34 +53,51 @@ def email_detail(message_id):
 @app.route('/emails')
 @app.route('/emails/')
 def email_list(query=None, msg_id=None):
-    # if no search, return all results, limit to 200 for debugging purposes
+
     flds = config.LIST_VIEW_FIELDS
     query = request.args.get('search')
+
+# if no search, return all results, limit to 200 for debugging purposes
     if query is None:
         msgs = emails.find(fields=flds, limit=200)
         total = msgs.count()
+
+# execute basic keyword search
     elif request.args.get('search'):
         results = es.search(index='test_kaminski', doc_type='email', body={'query': { 'query_string': { 'default_field': 'body', 'query': query }}, 'sort': {'_score': {'order': 'desc'}}}, size=100, _source=True, analyze_wildcard=True, default_operator='AND') 
         total = results['hits']['total']
         msgs = es_to_dict(results)
-    elif request.args['search_type'] is 'adv_search':
-        results = es.search(index='test_kaminski', doc_type='email', body={'query': {'more_like_this': { 'fields': ['Subject', 'body'], 'like_text': query, 'percent_terms_to_match': 0.6, 'min_term_freq': 1, 'min_doc_freq': 1, 'max_query_terms': 12, 'prefix_length': 4, 'boost': 1.2 }}, 'sort': {'_score': {'order': 'desc' }}}, size=100, _source=True, analyze_wildcard=True)
-        total = results['hits']['total']
-        msgs = es_to_dict(results)
-    elif request.args['msg_id'] is not None:
-        #results = es.mlt(index='test_kaminski', doc_type='email', id=msg_id)
-        msg = emails.find_one({'_id': msg_id })
-        query = msg['body']
-        results = es.search(index='test_kaminski', doc_type='email', body={'query': {'more_like_this': { 'fields': ['body'], 'like_text': query, 'percent_terms_to_match': 0.6, 'min_term_freq': 1, 'min_doc_freq': 1, 'max_query_terms': 12, 'prefix_length': 4, 'boost': 1.2 }}, 'sort': {'_score': {'order': 'desc' }}}, size=100, _source=True, analyze_wildcard=True)
+
+    return render_template('list.html', msgs=msgs, flds=flds, total=total, query=query)
+
+@app.route('/emails/adv_search')
+def email_adv_search(query=None):
+
+    query = request.args.get('search')
+    
+# execute advanced boolean search
+    if request.args.get('search'):
+        results = es.search(index='test_kaminski', doc_type='email', body={'query': {'more_like_this': { 'fields': ['Subject', 'body'], 'like_text': query, 'percent_terms_to_match': 0.6, 'min_term_freq': 1, 'min_doc_freq': 1, 'max_query_terms': 12, 'boost': 1.2 }}, 'sort': {'_score': {'order': 'desc' }}}, size=100, _source=True, analyze_wildcard=True)
         total = results['hits']['total']
         msgs = es_to_dict(results)
 
-    # idea here is to add GET parameter so it looks like /emails?search_type=advanced_search&search=query but it's not working
-    #elif request.args['search_type'] is 'adv_search':
-    #    results = es.search({'query': {'query_string': {'default_field': 'body', 'query': query}}, 'sort': {'_score': {'order': 'desc'}}}, index='test_kaminski')
-    #    total = results['hits']['total']
-    #    msgs = es_to_dict(results)
-    return render_template('list.html', msgs=msgs, flds=flds, total=total, query=query)
+    return render_template('list.html', msgs=msgs, total=total, query=query)
+
+@app.route('/emails/mlt')
+def email_mlt(query=None, msg_id=None):
+
+    query = request.args.get('search')
+    msg = emails.find_one({'_id': msg_id })
+
+# execute More Like This search of single doc to find similar docs
+# Note: In order to use the mlt feature a mlt_field needs to be either be stored, store term_vector or source needs to be enabled.
+    if request.args.get('search'):
+        results = es.mlt(index='test_kaminski', doc_type='email', id=msg_id)
+        #query = msg['body']
+        total = results['hits']['total']
+        msgs = es_to_dict(results)
+
+    return render_template('list.html', msgs=msgs, total=total, query=query)
 
 if __name__ == '__main__':
     app.run(debug=True)
