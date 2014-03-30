376e4ed2d601a4d113db4d2e10ca6a7197ffc0fa
Ben Cassedy <bencassedy@hotmail.com>
2014-03-30 14:39:15 -0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@
 # LIST_VIEW_FIELDS can be used to set email fields to display in list view.
 LIST_VIEW_FIELDS = ['Subject', 'From', 'To', 'Date', 'CC', 'BCC', 'Message-ID']
 CSRF_ENABLED = True
-SECRET_KEY = 'secret'
+SECRET_KEY = ''
 
 # ES settings
 INDEX = 'test_kaminski'
